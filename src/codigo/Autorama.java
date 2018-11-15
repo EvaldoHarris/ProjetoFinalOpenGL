cb16920767980c5e62bddb118b747649a2bed6d0
edugarcia98 <edugarcia758@gmail.com>
2018-11-15 14:55:58 -0200

    Simulando uma corrida

--- src/codigo/Autorama.java
+++ src/codigo/Autorama.java
@@ -15,6 +15,7 @@ import javax.media.opengl.GLEventListener;
 import javax.media.opengl.awt.GLJPanel;
 import javax.media.opengl.glu.GLU;
 import javax.swing.JFrame;
+import java.util.Random;
 
 /**
  *
@@ -25,16 +26,13 @@ public class Autorama
 
     GLU glu = new GLU();
     GLUT glut = new GLUT();
+    
+    private Carro cBlue = new Carro(); 
+    private Carro cRed = new Carro();
 
     public static void main(String args[]) {
         new Autorama();
     }
-    
-    private double gX, gY, gZ;
-    private double gR = 0;
-    private boolean reta1=true, reta2=false, reta3=false, reta4=false;
-    private int passou = 0;
-    private boolean alterna = false;
 
     public Autorama() {
         GLJPanel canvas = new GLJPanel();
@@ -61,7 +59,7 @@ public class Autorama
     public void init(GLAutoDrawable glAuto) {
         Animator a = new Animator(glAuto);
         a.start();
-        GL2 gl = glAuto.getGL().getGL2();
+        GL2 gl = glAuto.getGL().getGL2();    
         gl.glClearColor(0.4f, 0.4f, 0.4f, 0.4f);
         gl.glEnable(GL.GL_DEPTH_TEST);
            int x = -23, y = 0, z = 8;
@@ -93,151 +91,28 @@ public class Autorama
         
         desenhaPista(gl);
         
-        
-        gl.glTranslated(-3 + gX, gY, gZ);
-        gl.glRotated(gR, 0, 1, 0);
-        
         //criar método mover carro, adicionar push e pop neles e fazer um diferente para cada carro
-        if(reta1 == true)
-        {
-            if (gX < 8) {
-                gX += 0.02;
-                gY = 0;
-                gZ = 0;  
-            } 
-            else {
-                gX = 8;
-                if(gZ <1)
-                {
-                    gZ += 0.005;
-                    gR -= 0.1;
-                }
-                else if(gZ >= 1 && gZ < 2)
-                {
-                    gZ += 0.1;
-                    gR -= 0.2;   
-                }
-                if(gR > -90 && gZ >= 2)
-                {
-                    reta1 = false;
-                    reta2 = true;
-                }
-            }
-        }
-        
-        if(reta2 == true)
-        {
-            if(gR > -90 && gZ >= 2 && passou == 0)
-            {
-                gl.glTranslated(-7, 0, -3);
-                gl.glRotated(-90-gR,0,1,0);
-                gl.glRotated(0,0,1,0);
-            }
+        gl.glPushMatrix();
+            percurso(gl,cBlue);
+            desenhaCarro(gl, glut, 0, 0, 1);
+        gl.glPopMatrix();
             
-            if(gZ < 11.3 && passou == 0)
-                gZ += 0.02;
-            else if(gZ >= 11.3 && gZ <= 11.32 && passou == 0)
-            {
-                gR -= 90;
-                gX -= 6;
-                gZ -= 7.5;
-                passou = 1;
-            }
-            else if(gZ >= 2.5 && gZ <= 3.9)
-            {
-                gZ -= 0.008;
-                gR -= 0.2;
-            }
-            
-            if(gR > -270 && gZ <= 2.5 && passou == 1)
-            {
-                reta2 = false;
-                reta3 = true;
-                passou = 0;
-            }
-        }
         
-        if(reta3 == true)
-        {
-           if(gR > -270 && gZ <= 2.5 && passou == 0)
-           {
-               gl.glTranslated(-4, 0, 0);
-               gl.glRotated(-180-gR, 0, 1, 0);
-           }
-           
-           if(gX > -7 && passou == 0)
-               gX -= 0.02;
-           else if(gX <= -7 && gX >= -7.02 && passou == 0)
-           {
-               gR -= 90;
-               gX += 8;
-               passou = 1;
-           }
-           else if(gX >= 0.9 && gX <= 1.8 && passou == 1)
-           {
-               gX += 0.005;
-               gR -= 0.2;
-           }
-           
-           if(gR > -270 && gX >= 1.8 && passou == 1)
-           {
-               reta3 = false;
-               reta4 = true;
-               passou = 0;
-           }
-        }
         
-        if(reta4 == true)
-        {
-            if(gR > -270 && gX >= 1.8 && passou == 0)
-            {
-               gl.glTranslated(-0.25, 0, 0.4);
-               gl.glRotated(90-gR, 0, 1, 0);
-            }
-            
-            if(gZ > -7 && passou == 0)
-                gZ -= 0.02;
-            else if(gZ <= -7 && gZ >= -7.02 && passou == 0)
-            {
-                gR -= 60;
-                gX -= 1.8;
-                gZ += 7;
-                passou = 1;
-            }
-            else if(gZ > -0.5 && passou == 1)
-            {
-                gZ -= 0.005;
-                gR -= 0.2;
-            }
-            
-            if(gZ <= -0.5 && passou == 1)
-            {
-                gR = gX = gY = gZ = 0;
-                passou = 0;
-                //gl.glTranslated(5, 0, 0);
-                //gl.glRotated(gR, 0, 1, 0);
-                reta4 = false;
-                reta1 = true;
-            }
-            
-            //TODO: resetar o caminho ou colocar algo para que ele refaça a volta
-        }
-        
-        desenhaCarro(gl, glut, 0, 0, 1);
-
-        gl.glTranslated(0, 0, 1);
-        
-        desenhaCarro(gl, glut, 1, 0, 0);
+        gl.glPushMatrix();
+            if(cRed.reta1 == true)
+                gl.glTranslated(0, 0, 1);
+            if(cRed.reta2 == true)
+                gl.glTranslated(-1, 0, 0);
+            if(cRed.reta3 == true)
+                gl.glTranslated(0, 0, -1);
+            if(cRed.reta4 == true)
+                gl.glTranslated(1, 0, 0);
+            percurso(gl,cRed);
+            desenhaCarro(gl, glut, 1, 0, 0);
+        gl.glPopMatrix();
     }
 
-//    private void desenhaTabuleiro(GL2 gl) {
-//        gl.glPushMatrix();
-//        for (int i = 0; i < 8; i++) {
-//            desenhaLinha(gl, i % 2);
-//            gl.glTranslated(0, 2, 0);
-//        }
-//        gl.glPopMatrix();
-//    }
     public void reshape(GLAutoDrawable gLAutoDrawable, int x, int y, int w, int h) {
 
         GL2 gl = gLAutoDrawable.getGL().getGL2();
@@ -302,46 +177,6 @@ public class Autorama
             gl.glEnd();
         }
         gl.glPopMatrix();
-
-        /*gl.glBegin(GL2.GL_QUADS);
-         gl.glColor3f(1,0,0);
-         gl.glVertex3d(-1, -1, -1);
-         gl.glVertex3d(1, -1, -1);
-         gl.glVertex3d(1, -1, 1);
-         gl.glVertex3d(-1, -1, 1);
-         gl.glEnd();
-            
-         gl.glBegin(GL2.GL_QUADS);
-         gl.glColor3f(0, 1, 0);
-         gl.glVertex3d(-1, 1, -1);
-         gl.glVertex3d( 1, 1, -1);
-         gl.glVertex3d(1, -1, -1);
-         gl.glVertex3d(-1, -1, -1);
-         gl.glEnd();
-            
-         gl.glBegin(GL2.GL_QUADS);
-         gl.glColor3f(0, 0, 1);
-         gl.glVertex3d(-1, 1, 1);
-         gl.glVertex3d(1, 1, 1);
-         gl.glVertex3d(1, -1, 1);
-         gl.glVertex3d(-1, -1, 1);
-         gl.glEnd();
-            
-         gl.glBegin(GL2.GL_QUADS);
-         gl.glColor3f(1, 0, 1);
-         gl.glVertex3d(1, 1, -1);
-         gl.glVertex3d(1, 1, 1);
-         gl.glVertex3d(1, -1, 1);
-         gl.glVertex3d(1, -1, -1);
-         gl.glEnd();
-            
-         gl.glBegin(GL2.GL_QUADS);
-         gl.glColor3f(0, 1, 1);
-         gl.glVertex3d(-1, 1, -1);
-         gl.glVertex3d(-1, 1, 1);
-         gl.glVertex3d(-1, -1, 1);
-         gl.glVertex3d(-1, -1, -1);
-         gl.glEnd();*/
     }
 
     public void desenhaCarro(GL2 gl, GLUT glut, double r, double g, double b) {
@@ -610,26 +445,134 @@ public class Autorama
         gl.glPopMatrix();
     }
 
-//    private void desenhaQuadrado(int cor, GL2 gl) {
-//        gl.glColor3f(cor, cor, cor);
-//        gl.glBegin(GL2.GL_QUADS);
-//        gl.glVertex3d(1, 1, 0);
-//        gl.glVertex3d(-1, 1, 0);
-//        gl.glVertex3d(-1, -1, 0);
-//        gl.glVertex3d(1, -1, 0);
-//        gl.glEnd();
-//
-//    }
-//    private void desenhaLinha(GL2 gl, int start) {
-//        gl.glPushMatrix();
-//        for (int i = 0; i < 8; i++) {
-//            if (start == 0) {
-//                desenhaQuadrado(i % 2, gl);
-//            } else {
-//                desenhaQuadrado((i + 1) % 2, gl);
-//            }
-//            gl.glTranslated(2, 0, 0);
-//        }
-//        gl.glPopMatrix();
-//    }
+    public void percurso(GL2 gl, Carro c)
+    {
+        Random rand = new Random();
+        int randomValue = rand.nextInt(9 + 1) + 1;
+        gl.glTranslated(-3 + c.gX, c.gY, c.gZ);
+        gl.glRotated(c.gR, 0, 1, 0);
+        
+        if(c.reta1 == true)
+        {
+            if (c.gX < 8) {
+                c.gX += 0.004 * randomValue;
+                c.gY = 0;
+                c.gZ = 0;  
+            } 
+            else {
+                c.gX = 8;
+                if(c.gZ <1)
+                {
+                    c.gZ += 0.005;
+                    c.gR -= 0.1;
+                }
+                else if(c.gZ >= 1 && c.gZ < 2)
+                {
+                    c.gZ += 0.1;
+                    c.gR -= 0.2;   
+                }
+                if(c.gR > -90 && c.gZ >= 2)
+                {
+                    c.reta1 = false;
+                    c.reta2 = true;
+                }
+            }
+        }
+        
+        if(c.reta2 == true)
+        {
+            if(c.gR > -90 && c.gZ >= 2 && c.passou == 0)
+            {
+                gl.glTranslated(-7, 0, -3);
+                gl.glRotated(-90-c.gR,0,1,0);
+                gl.glRotated(0,0,1,0);
+            }
+            
+            if(c.gZ < 11.3 && c.passou == 0)
+                c.gZ += 0.004 * randomValue;
+            else if(c.gZ >= 11.3 && c.gZ <= 11.4 && c.passou == 0)
+            {
+                c.gR -= 90;
+                c.gX -= 6;
+                c.gZ -= 7.5;
+                c.passou = 1;
+            }
+            else if(c.gZ >= 2.5 && c.gZ <= 3.9)
+            {
+                c.gZ -= 0.008;
+                c.gR -= 0.2;
+            }
+            
+            if(c.gR > -270 && c.gZ <= 2.5 && c.passou == 1)
+            {
+                c.reta2 = false;
+                c.reta3 = true;
+                c.passou = 0;
+            }
+        }
+        
+        if(c.reta3 == true)
+        {
+           if(c.gR > -270 && c.gZ <= 2.5 && c.passou == 0)
+           {
+               gl.glTranslated(-4, 0, 0);
+               gl.glRotated(-180-c.gR, 0, 1, 0);
+           }
+           
+           if(c.gX > -7 && c.passou == 0)
+               c.gX -= 0.004 * randomValue;
+           else if(c.gX <= -7 && c.gX >= -7.1 && c.passou == 0)
+           {
+               c.gR -= 90;
+               c.gX += 8;
+               c.passou = 1;
+           }
+           else if(c.gX >= 0.9 && c.gX <= 1.8 && c.passou == 1)
+           {
+               c.gX += 0.0048;
+               c.gR -= 0.2;
+           }
+           
+           if(c.gR > -270 && c.gX >= 1.8 && c.passou == 1)
+           {
+               c.reta3 = false;
+               c.reta4 = true;
+               c.passou = 0;
+           }
+        }
+        
+        if(c.reta4 == true)
+        {
+            if(c.gR > -270 && c.gX >= 1.8 && c.passou == 0)
+            {
+               gl.glTranslated(-0.25, 0, 0.4);
+               gl.glRotated(90-c.gR, 0, 1, 0);
+            }
+            
+            if(c.gZ > -7 && c.passou == 0)
+                c.gZ -= 0.004 * randomValue;
+            else if(c.gZ <= -7 && c.gZ >= -7.1 && c.passou == 0)
+            {
+                c.gR -= 60;
+                c.gX -= 1.8;
+                c.gZ += 7;
+                c.passou = 1;
+            }
+            else if(c.gZ > -0.5 && c.passou == 1)
+            {
+                c.gZ -= 0.005;
+                c.gR -= 0.2;
+            }
+            
+            if(c.gZ <= -0.5 && c.passou == 1)
+            {
+                c.gR = c.gX = c.gY = c.gZ = 0;
+                c.passou = 0;
+                //gl.glTranslated(5, 0, 0);
+                //gl.glRotated(gR, 0, 1, 0);
+                c.reta4 = false;
+                c.reta1 = true;
+            }
+        }
+    }
 }
---
+++ src/codigo/Carro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author T-Gamer
+ */
+public class Carro {
+    double gX;
+    double gY;
+    double gZ;
+    double gR;
+    boolean reta1;
+    boolean reta2;
+    boolean reta3;
+    boolean reta4;
+    int passou;
+
+    public Carro() {
+        gR = 0;
+        reta1 = true;
+        reta2 = false;
+        reta3 = false;
+        reta4 = false;
+        passou = 0;
+    }
+}
